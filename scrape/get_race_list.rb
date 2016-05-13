96897052654975e55f9d38413b578653599bb614
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-05-13 20:11:44 +0900

    yesterday if nil

--- scrape/get_race_list.rb
+++ scrape/get_race_list.rb
@@ -4,8 +4,9 @@ require 'net/http'
 OUTPUT_DIR = File.join(File.expand_path(File.dirname('..')), 'data/races')
 BASE_URL = 'http://db.netkeiba.com/race/list'
 
-from = ARGV[0] ? ARGV[0] : '1988/01/05'
-to = ARGV[1] ? ARGV[1] : Time.now.strftime('%Y/%m/%d')
+today = Time.now.strftime('%Y/%m/%d')
+from = ARGV[0] ? ARGV[0] : (today - 24 * 60 * 60)
+to = ARGV[1] ? ARGV[1] : today
 (Date.parse(from)..Date.parse(to)).each do |date|
   parsed_url = URI.parse("#{BASE_URL}/#{date.strftime('%Y%m%d')}")
 
