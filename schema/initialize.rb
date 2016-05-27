08841d455752ebfc9b662132d3e68b4b703a9932
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-05-27 15:30:38 +0900

    update initialize.rb

--- schema/initialize.rb
+++ schema/initialize.rb
@@ -1,25 +1,22 @@
+require_relative '../config/settings.rb'
 require 'mysql2'
 
-DATABASE = 'alterf'
-
-query = "DROP DATABASE #{DATABASE}"
-client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "7QiSlC?4")
+mysql_conf = {:host => Settings.host, :username => Settings.username, :password => Settings.password}
+query = "DROP DATABASE #{Settings.database}"
+client = Mysql2::Client.new(mysql_conf)
 client.query(query)
-client.close
 
 query =<<"EOF"
 CREATE DATABASE IF NOT EXISTS
-  #{DATABASE}
+  #{Settings.database}
 DEFAULT CHARACTER SET
   utf8
 EOF
-client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "7QiSlC?4")
 client.query(query)
 client.close
 
-client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "7QiSlC?4", :database => DATABASE)
-Dir[File.join(File.expand_path(File.dirname(__FILE__)), '*.sql')].each do |sql_file|
-  query = File.read(sql_file)
-  client.query(query)
+client = Mysql2::Client.new(mysql_conf.merge(:database => Settings.database))
+Dir[File.join(Settings.application_root, 'schema/*.sql')].each do |sql_file|
+  client.query(File.read(sql_file))
 end
 client.close
