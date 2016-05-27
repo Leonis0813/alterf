48004099594dc60cd9d4d2e739bb9622ebbf1be6
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-05-27 13:49:43 +0900

    modify schema

--- mysql/race_horses.sql
+++ mysql/race_horses.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS race_horses (
   race_id INTEGER,
   horse_id INTEGER,
+  number INTEGER NOT NULL,
+  bracket INTEGER NOT NULL,
   age INTEGER NOT NULL,
   jockey VARCHAR(16) NOT NULL,
   burden_weight FLOAT NOT NULL,
--- mysql/race_results.sql
+++ mysql/race_results.sql
@@ -1,13 +1,12 @@
 CREATE TABLE IF NOT EXISTS race_results (
   race_id INTEGER,
   horse_id INTEGER,
-  number INTEGER NOT NULL,
   `order` INTEGER NOT NULL,
-  time TIME NOT NULL,
+  time FLOAT NOT NULL,
   margin VARCHAR(10) NOT NULL,
   third_corner INTEGER NOT NULL,
   forth_corner INTEGER NOT NULL,
-  slope TIME NOT NULL,
+  slope FLOAT NOT NULL,
   odds FLOAT NOT NULL,
   popularity INTEGER NOT NULL,
   PRIMARY KEY(race_id, horse_id)
