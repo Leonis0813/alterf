0aa5381f351c0da2949fa04d06692911e7bd5171
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-05-27 15:56:47 +0900

    modify fetch files

--- fetch/race_list.rb
+++ fetch/race_list.rb
@@ -1,20 +1,24 @@
+require_relative '../config/settings.rb'
 require 'date'
+require 'fileutils'
 require 'net/http'
 
-OUTPUT_DIR = File.join(File.expand_path(File.dirname('..')), 'data/races')
-BASE_URL = 'http://db.netkeiba.com/race/list'
+OUTPUT_DIR = File.join(Settings.application_root, 'raw_data/races')
 
 today = Time.now.strftime('%Y/%m/%d')
 from = ARGV[0] ? ARGV[0] : (today - 24 * 60 * 60)
 to = ARGV[1] ? ARGV[1] : today
-(Date.parse(from)..Date.parse(to)).each do |date|
-  parsed_url = URI.parse("#{BASE_URL}/#{date.strftime('%Y%m%d')}")
 
-  res = Net::HTTP.start(parsed_url.host, parsed_url.port) {|http| http.request Net::HTTP::Get.new(parsed_url) }
+(Date.parse(from)..Date.parse(to)).each do |date|
+  parsed_url = URI.parse("#{Settings.url}/#{date.strftime('%Y%m%d')}")
+
+  res = Net::HTTP.start(parsed_url.host, parsed_url.port) do |http|
+    http.request Net::HTTP::Get.new(parsed_url)
+  end
 
   races = res.body.scan(%r[.*(/race/\d+)]).flatten
   unless races.empty?
-    Dir.mkdir(OUTPUT_DIR) unless File.exist?(OUTPUT_DIR)
+    FileUtils.mkdir_p(OUTPUT_DIR)
     File.open(File.join(OUTPUT_DIR, "#{date.strftime('%Y%m%d')}.txt"), 'w') do |out|
       races.each {|race| out.puts(race) }
     end
--- fetch/race_result.rb
+++ fetch/race_result.rb
@@ -1,19 +1,21 @@
-require 'date'
+require_relative '../config/settings.rb'
+require 'fileutils'
 require 'net/http'
 
-APPLICATION_ROOT = File.expand_path(File.dirname('..'))
-OUTPUT_DIR = File.join(APPLICATION_ROOT, 'data/results')
-BASE_URL = 'http://db.netkeiba.com'
-
-Dir.mkdir(OUTPUT_DIR) unless File.exist?(OUTPUT_DIR)
+OUTPUT_DIR = File.join(Settings.application_root, 'raw_data/results')
 
 filename = ARGV[0] ? ARGV[0] : '*'
-Dir[File.join(APPLICATION_ROOT, 'data/races', "#{filename}.txt")].sort.each do |file|
+
+FileUtils.mkdir_p(OUTPUT_DIR)
+
+Dir[File.join(Settings.application_root, 'raw_data/races', "#{filename}.txt")].sort.each do |file|
   File.open(file, 'r') do |file|
     file.each_line do |path|
-      parsed_url = URI.parse("#{BASE_URL}#{path.chomp}")
+      parsed_url = URI.parse("#{Settings.url}#{path.chomp}")
 
-      res = Net::HTTP.start(parsed_url.host, parsed_url.port) {|http| http.request Net::HTTP::Get.new(parsed_url) }
+      res = Net::HTTP.start(parsed_url.host, parsed_url.port) do |http|
+        http.request Net::HTTP::Get.new(parsed_url)
+      end
 
       File.open(File.join(OUTPUT_DIR, "#{path.match(/\/race\/(\d+)/)[1]}.html"), "w:utf-8") do |out|
         res.body.encode("utf-8", "euc-jp").split("\n").each do |line|
