a54e58bd0366a66c5d4f2e051eddd89592754ed5
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-05-28 11:38:26 +0900

    import horse

--- import/horse.rb
+++ import/horse.rb
@@ -1,6 +1,7 @@
 require_relative '../config/settings.rb'
+require_relative '../model/horse.rb'
 
 def import_horse(file_id)
   horse_file = File.join(Settings.application_root, 'raw_data/horses', "#{file_id}.html")
-  
+  Horse.new(html_file)
 end
