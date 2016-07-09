ce796ab01cee101769549d57360745233e91cba3
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-07-09 20:40:08 +0900

    add entry parser

--- helper/html.rb
+++ helper/html.rb
@@ -34,7 +34,21 @@ class HTML
     end
 
     def entry(html)
+      entries = html.scan(/<table class="race_table.*?<\/table>/).first.scan(/<tr>.*?<\/tr>/)
 
+      entries.map do |entry|
+        features = entry.gsub(/<[\/]?tr>/, '').scan(/<td.*?>(.*?)<\/td>/).flatten
+        features.map! {|feature| feature.gsub(/<.*?>/, '') }
+
+        {}.tap do |attribute|
+          attribute[:number] = features[2].to_i
+          attribute[:bracket] = features[1].to_i
+          attribute[:age] = features[4].match(/(\d+)\z/)[1].to_i
+          attribute[:jockey] = features[6]
+          attribute[:burden_weight] = features[5].to_f
+          attribute[:weight] = features[14].match(/\A(\d+)/)[1].to_f unless features[14] == '計不'
+        end
+      end
     end
 
     def result(html)
