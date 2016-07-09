0ef5dd7d902725f5e54a0a3eb796bc1160c7d96d
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-07-09 20:19:46 +0900

    add race parser

--- helper/html.rb
+++ helper/html.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class HTML
   class << self
     def parse(obj, html)
@@ -7,7 +8,29 @@ class HTML
     private
 
     def race(html)
+      race_data = html.scan(/<dl class="racedata.*?\/dl>/).first
 
+      {}.tap do |attribute|
+        attribute[:name] = race_data.match(/<h1>(.*)<\/h1>/)[1].gsub(/<.*?>/, '').strip
+
+        condition = race_data.match(/<span>(.*)<\/span>/)[1].split(' / ')
+        attribute[:track] = condition.first[0].sub('ダ', 'ダート')
+        attribute[:direction] = condition.first[1]
+        attribute[:distance] = condition.first.match(/(\d*)m$/)[1].to_i
+        attribute[:weather] = condition[1].match(/天候 : (.*)/)[1]
+        attribute[:track_condition] = condition[2].match(/[ダート|芝] : (.*)/)[1]
+
+        start_time = condition[3].match(/発走 : (.*)/)[1]
+        race_date = html.match(/<li class="result_link"><.*?>(\d*年\d*月\d*日)のレース結果<.*?>/)[1]
+        race_date = race_date.gsub(/年|月/, '-').sub('日', '')
+        attribute[:start_time] = "#{race_date} #{start_time}:00"
+
+        place = html.scan(/<ul class="race_place.*?<\/ul>/).first
+        attribute[:place] = place.match(/<a href=.* class="active">(.*?)<\/a>/)[1]
+        attribute[:round] = race_data.match(/<dt>(\d*) R<\/dt>/)[1].to_i
+        horses = html.scan(/<table class="race_table.*?<\/table>/).first
+        attribute[:num_of_horse] = horses.scan(/<tr>.*?<\/tr>/).size
+      end
     end
 
     def entry(html)
@@ -21,4 +44,5 @@ class HTML
     def payoff(html)
 
     end
+  end
 end
