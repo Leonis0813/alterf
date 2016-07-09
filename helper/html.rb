1b7ea9a8820cce47cd14953e5da483da0aa8274b
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-07-09 16:16:00 +0900

    parse result first

---
+++ helper/html.rb
@@ -0,0 +1,24 @@
+class HTML
+  class << self
+    def parse(obj, html)
+      send(obj, html)
+    end
+
+    private
+
+    def race(html)
+
+    end
+
+    def entry(html)
+
+    end
+
+    def result(html)
+
+    end
+
+    def payoff(html)
+
+    end
+end
--- import/race.rb
+++ import/race.rb
@@ -6,30 +6,32 @@ def import_race(race_id)
   html_file = File.join(Settings.backup_path, "races/#{race_id}.html")
   html = File.read(html_file).gsub("\n", '').gsub('&nbsp;', ' ')
 
-  parsed_race = HTML.parse(html, :race)
+  parsed_race = HTML.parse(:race, html)
   race = Race.new(parsed_race)
   race.save!
 
-  parsed_entries = HTML.parse(html, :entry)
-  parsed_entries.each do |e|
-    entry = Entry.new(e)
+  parsed_entries = HTML.parse(:entry, html)
+  parsed_results = HTML.parse(:result, html)
+  parsed_entries.zip(parsed_results).each_with_index do |parsed_data, i|
+    entry = Entry.new(parsed_data.first)
+    result = Result.new(parsed_data.last)
+
     horse = Horse.find_by(:external_id => entry.external_id)
     horse = unless horse
               output_horse(entry.external_id)
               import_horse(entry.external_id)
             end
+
     entry.race_id = race.id
     entry.horse_id = horse.id
     entry.save!
 
-    parsed_result = HTML.parse(html, :result)
-    result = Result.new(parsed_result)
     result.race_id = race.id
     result.horse_id = horse.id
     result.save!
   end
 
-  parsed_payoffs = HTML.parse(html, :payoff)
+  parsed_payoffs = HTML.parse(:payoff, html)
   parsed_payoffs.each do |p|
     payoff = Payoff.new(p)
     payoff.race_id = race.id
