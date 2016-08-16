edf0db8dce72be9058362ddc808c9bb429cb2962
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-08-16 11:04:44 +0900

    extract/race_list use logger

--- import/extract/race_list.rb
+++ import/extract/race_list.rb
@@ -1,3 +1,11 @@
+require_relative '../../lib/logger'
+
 def parse_race_list(html)
-  html.scan(%r[.*/race/(\d+)]).flatten
+  races = html.scan(%r[.*/race/(\d+)]).flatten
+  Logger.write(
+    File.basename(__FILE__, '.rb'),
+    'extract',
+    {:'#_of_races' => races.size, :from => races.first, :to => races.last}
+  )
+  races
 end
