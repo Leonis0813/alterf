6ada22881195985739ca8aaec499a8a477493176
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-08-16 10:03:32 +0900

    resource_type to resource

--- import/insert.rb
+++ import/insert.rb
@@ -1,8 +1,8 @@
 require 'mysql2'
 require_relative '../config/settings'
 
-def insert(resource_type, attribute)
-  query = File.read(File.join(Settings.application_root, 'import/insert', "#{resource_type}.sql"))
+def insert(resource, attribute)
+  query = File.read(File.join(Settings.application_root, 'import/insert', "#{resource}.sql"))
   attribute.each {|key, value| query.gsub!("$#{key.upcase}") {value.to_s} }
   client = Mysql2::Client.new(Settings.mysql)
   begin
--- import/output.rb
+++ import/output.rb
@@ -1,9 +1,9 @@
 require 'fileutils'
 require_relative '../config/settings'
 
-def output(resource_type, string, file_name)
-  FileUtils.mkdir_p(Settings.backup_dir[resource_type])
-  file_path = File.join(Settings.backup_dir[resource_type], file_name)
+def output(resource, string, file_name)
+  FileUtils.mkdir_p(Settings.backup_dir[resource])
+  file_path = File.join(Settings.backup_dir[resource], file_name)
   File.open(file_path, 'w') {|out| out.write(string) }
   file_path
 end
