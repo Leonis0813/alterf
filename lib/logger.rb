687ea0c2970aaa6d65e1e06b07f917eab1eb53c7
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-10-14 20:46:17 +0900

    output log if file exists

--- lib/logger.rb
+++ lib/logger.rb
@@ -4,12 +4,12 @@ require_relative '../config/settings'
 module Logger
   def self.write(resource, operate, text)
     file_path = File.join(Settings.application_root, "log/#{operate}.log")
-    body = [
-      "[#{Time.now.strftime('%Y-%m-%d %H:%M:%S.%6N')}]",
-      "[#{resource}]",
-      text.to_json,
-    ].join(' ')
     File.open(file_path, 'a') do |file|
+      body = [
+        "[#{Time.now.strftime('%Y-%m-%d %H:%M:%S.%6N')}]",
+        "[#{resource}]",
+        text.to_json,
+      ].join(' ')
       file.puts(body)
     end
   end
