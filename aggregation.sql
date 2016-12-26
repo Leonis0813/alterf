80923510b8fe809b10bd36e2d1ee2e89b42725d4
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2016-12-26 23:16:38 +0900

    join parent horse data

--- aggregation.sql
+++ aggregation.sql
@@ -38,9 +38,22 @@ LEFT JOIN
   races AS race
 ON
   entry.race_id = race.id
-LEFT JOIN
-  horse AS horse
-ON
+LEFT JOIN (
+  SELECT
+    `self`.*,
+    father.name AS father_horse_name,
+    mother.name AS mother_horse_name,
+  FROM
+    horse AS `self`
+  LEFT JOIN
+    horses AS father
+  ON
+    `self`.father_id = father.id
+  LEFT JOIN
+    horses AS mother
+  ON
+    `self`.mother_id = mother.id
+) AS horse ON
   entry.horse_id = horse.id
 LEFT JOIN
   results AS result
