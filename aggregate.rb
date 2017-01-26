dc811e22fd528ffbb566caf916d2bbc27f12462e
Leonis0813 <Leonis.0813@gmail.com>
2017-01-26 13:30:41 +0000

    aggregation to aggregate

--- aggregate.rb
+++ aggregate.rb
@@ -3,5 +3,5 @@ require 'mysql2'
 require_relative 'config/settings'
 
 client = Mysql2::Client.new(Settings.mysql)
-query = File.read(File.join(Settings.application_root, 'aggregation.sql'))
+query = File.read(File.join(Settings.application_root, 'aggregate.sql'))
 client.query(query)
