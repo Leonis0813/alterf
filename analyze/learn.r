d546ec17f6265d56d0d6e4d7ae22e759e96591bf
Leonis0813 <Leonis.0813@gmail.com>
2017-04-16 05:17:32 +0100

    add argument

--- analyze/learn.r
+++ analyze/learn.r
@@ -1,4 +1,8 @@
+args = commandArgs(trailingOnly=T)
+num_training_data <- args[1]
+
 library(RMySQL)
 driver <- dbDriver("MySQL")
 dbconnector <- dbConnect(driver, dbname="alterf", user="root", password="7QiSlC?4", host="160.16.66.112", port=3306)
-training_data <- dbGetQuery(dbconnector, "SELECT * FROM training_data LIMIT 10000")
+
+training_data <- dbGetQuery(dbconnector, paste("SELECT * FROM training_data LIMIT ", num_training_data))
