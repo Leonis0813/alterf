9a23c64848d7876e45deea828b13b279e4a08962
Leonis0813 <Leonis.0813@gmail.com>
2017-04-22 05:26:01 +0100

    install random forest

--- analyze/learn.r
+++ analyze/learn.r
@@ -6,3 +6,5 @@ driver <- dbDriver("MySQL")
 dbconnector <- dbConnect(driver, dbname="alterf", user="root", password="7QiSlC?4", host="160.16.66.112", port=3306)
 
 training_data <- dbGetQuery(dbconnector, paste("SELECT * FROM training_data LIMIT ", num_training_data))
+
+library(randomForest)
