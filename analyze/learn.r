98233a48cbdd42096777ac80c810aa880988c514
Leonis0813 <Leonis.0813@gmail.com>
2017-04-23 00:10:24 +0900

    change to classification

--- analyze/learn.r
+++ analyze/learn.r
@@ -1,15 +1,14 @@
 args = commandArgs(trailingOnly=T)
 num_training_data <- args[1]
-ntree <- args[2]
-mtry <- args[3]
+ntree <- as.integer(args[2])
+mtry <- as.integer(args[3])
 
 library(RMySQL)
 driver <- dbDriver("MySQL")
 dbconnector <- dbConnect(driver, dbname="alterf", user="root", password="7QiSlC?4", host="160.16.66.112", port=3306)
 
-training_data <- dbGetQuery(dbconnector, paste("SELECT distance, round, number, bracket, age, burden_weight, weight, `order` FROM training_data WHERE `order` REGEXP '[0-9]+' LIMIT ", num_training_data))
-summary(training_data)
-mode(training_data$order) <- "integer"
+training_data <- dbGetQuery(dbconnector, paste("SELECT distance, round, number, bracket, age, burden_weight, weight, IF(`order` = '1', 1, 0) AS won FROM training_data WHERE `order` REGEXP '[0-9]+' LIMIT", num_training_data))
+training_data$won <- as.factor(training_data$won)
 
 library(randomForest)
-model <- randomForest(formula=order~., data=training_data, ntree=as.integer(ntree), mtry=as.integer(mtry))
+model <- randomForest(won~., data=training_data, ntree=ntree, mtry=mtry)
