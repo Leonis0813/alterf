0886aeeadf417d96329c8c964353353774576f91
Leonis0813 <Leonis.0813@gmail.com>
2017-04-22 14:38:43 +0900

    execute analyze

--- analyze/learn.r
+++ analyze/learn.r
@@ -1,10 +1,15 @@
 args = commandArgs(trailingOnly=T)
 num_training_data <- args[1]
+ntree <- args[2]
+mtry <- args[3]
 
 library(RMySQL)
 driver <- dbDriver("MySQL")
 dbconnector <- dbConnect(driver, dbname="alterf", user="root", password="7QiSlC?4", host="160.16.66.112", port=3306)
 
-training_data <- dbGetQuery(dbconnector, paste("SELECT * FROM training_data LIMIT ", num_training_data))
+training_data <- dbGetQuery(dbconnector, paste("SELECT distance, round, number, bracket, age, burden_weight, weight, `order` FROM training_data WHERE `order` REGEXP '[0-9]+' LIMIT ", num_training_data))
+summary(training_data)
+mode(training_data$order) <- "integer"
 
 library(randomForest)
+model <- randomForest(formula=order~., data=training_data, ntree=as.integer(ntree), mtry=as.integer(mtry))
