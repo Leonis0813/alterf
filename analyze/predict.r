d84adadd5babe9599854ac6cb8a6a3a9d2ad6caf
Leonis <Leonis.0813@gmail.com>
2017-06-17 16:00:51 +0900

    Predict race (#30)
    * predict race by model
    
    * save as yaml
    
    * output prediction result
    
    * create CHANGELOG.md

--- analyze/predict.r
+++ analyze/predict.r
@@ -16,3 +16,11 @@ rownames(entries) <- c("number", "bracket", "age", "burden_weight", "weight")
 entries <- as.data.frame(t(entries))
 entries$distance <- c(rep(test_data$distance, entry_size))
 entries$round <- c(rep(test_data$round, entry_size))
+
+library(randomForest)
+result <- predict(model, entries)
+
+timestamp <- format(Sys.time(), "%Y%m%d%H%M%S")
+for(i in 1:length(result)) {
+    write(paste(i, ": ", result[i], sep=""), file=paste("results/prediction_", timestamp, ".txt", sep=""), append=T)
+}
