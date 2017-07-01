3ee7fcfd6f69aeab2e5969e02f8edfc0d7aad4bc
Leonis0813 <Leonis.0813@gmail.com>
2017-07-01 19:25:24 +0900

    change method name to info and write_with_runtime

--- lib/logger.rb
+++ lib/logger.rb
@@ -2,15 +2,20 @@ require 'json'
 require_relative '../config/settings'
 
 module Logger
-  def self.write(resource, operate, text)
-    file_path = File.join(Settings.application_root, "log/#{operate}.log")
-    File.open(file_path, 'a') do |file|
-      body = [
-        "[#{Time.now.strftime('%Y-%m-%d %H:%M:%S.%6N')}]",
-        "[#{resource}]",
-        text.to_json,
-      ].join(' ')
-      file.puts(body)
+  FILE_PATH = File.join(Settings.application_root, 'log/aggregate.log')
+
+  class << self
+    def info(body)
+      body = ['[I]', "[#{Time.now.strftime('%F %T.%6N')}]", body.to_json].join('')
+      File.open(FILE_PATH, 'a') {|file| file.puts(body) }
+    end
+
+    def write_with_runtime(body)
+      start_time = Time.now
+      result = yield
+      end_time = Time.now
+      info body.merge(:runtime => end_time - start_time)
+      result
     end
   end
 end
