cf6c748f9f18cf16493311141007a2d0511ef3b5
Leonis0813 <Leonis.0813@gmail.com>
2017-07-09 03:54:26 +0100

    modify method name and params

--- import/fetch.rb
+++ import/fetch.rb
@@ -3,17 +3,16 @@ require_relative '../config/settings'
 require_relative '../lib/logger'
 
 def fetch(resource, id)
-  operate = File.basename(__FILE__, '.rb')
   files = Dir[File.join(Settings.backup_dir[resource], "#{id}.*")]
   if files.empty?
     parsed_url = URI.parse("#{Settings.url}#{Settings.path[resource]}/#{id}")
     res = Net::HTTP.start(parsed_url.host, parsed_url.port) do |http|
       http.request Net::HTTP::Get.new(parsed_url)
     end
-    Logger.write(resource, operate, {:status => res.code, :uri => res.uri.to_s})
+    Logger.info(:action => 'fetch', :status => res.code, :uri => res.uri.to_s)
     res.body
   else
-    Logger.write(resource, operate, {:message => 'already_exist', :path => files.first})
+    Logger.info(:action => 'fetch', :message => 'already_exist', :path => files.first)
     nil
   end
 end
--- import/output.rb
+++ import/output.rb
@@ -6,10 +6,12 @@ def output(resource, string, file_name)
   FileUtils.mkdir_p(Settings.backup_dir[resource])
   file_path = File.join(Settings.backup_dir[resource], file_name)
   File.open(file_path, 'w') {|out| out.write(string) }
-  Logger.write(
-    resource,
-    File.basename(__FILE__, '.rb'),
-    {:file_name => file_name, :length => string.length, :lines => string.lines.size}
+  Logger.info(
+    :action => 'output',
+    :resource => resource,
+    :file_name => file_name,
+    :length => string.length,
+    :lines => string.lines.size
   )
   file_path
 end
