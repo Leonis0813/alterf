433a5e8f0d9dcb80a2ce4dcb6346fa11ac6515b8
Leonis0813 <Leonis.0813@gmail.com>
2017-07-15 09:58:01 +0100

    puts if stdout is on

--- lib/logger.rb
+++ lib/logger.rb
@@ -8,11 +8,13 @@ module Logger
     def info(body)
       body = ['[I]', "[#{Time.now.strftime('%F %T.%6N')}]", body.to_json].join('')
       File.open(FILE_PATH, 'a') {|file| file.puts(body) }
+      puts body if ENV['STDOUT'].to_s == 'on'
     end
 
     def error(body)
       body = ['[E]', "[#{Time.now.strftime('%F %T.%6N')}]", body.to_json].join('')
       File.open(FILE_PATH, 'a') {|file| file.puts(body) }
+      puts body if ENV['STDOUT'].to_s == 'on'
     end
 
     def write_with_runtime(body)
