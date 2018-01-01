16dab28b51e4ce21b52b3dfc31f0cb5cb8d21dd6
Leonis0813 <Leonis.0813@gmail.com>
2018-01-01 13:20:34 +0000

    job to analyses

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180101101109) do
+ActiveRecord::Schema.define(version: 20180101125619) do
 
-  create_table "jobs", force: :cascade do |t|
-    t.integer  "num_data",    limit: 4,   null: false
-    t.integer  "num_tree",    limit: 4,   null: false
-    t.integer  "num_feature", limit: 4,   null: false
-    t.string   "state",       limit: 255, null: false
+  create_table "analyses", force: :cascade do |t|
+    t.integer  "num_data",    limit: 4
+    t.integer  "num_tree",    limit: 4
+    t.integer  "num_feature", limit: 4
+    t.string   "state",       limit: 255
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
   end
