8936d65c891b36d3a01d23aceb834c62bb7e9850
Leonis0813 <Leonis.0813@gmail.com>
2018-01-01 13:24:27 +0000

    create analysis object

--- app/jobs/analysis_job.rb
+++ app/jobs/analysis_job.rb
@@ -2,7 +2,10 @@ class AnalysisJob < ActiveJob::Base
   queue_as :default
 
   def perform(num_data, num_tree, num_feature)
+    job = Analysis.create!(:num_data => num_data, :num_tree => num_tree, :num_feature => num_feature, :state => 'processing')
     ret = system "Rscript #{Rails.root}/scripts/analyze/learn.r #{num_data} #{num_tree} #{num_feature}"
+    job[:state] = 'complete'
+    job.save!
     AnalysisMailer.finished(ret).deliver_now
   end
 end
