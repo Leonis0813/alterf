a59281ef15f279cd9e58d3bc144e562c3472923c
Leonis0813 <Leonis.0813@gmail.com>
2018-01-01 11:50:57 +0000

    create model

---
+++ app/models/job.rb
@@ -0,0 +1,7 @@
+class Job < ActiveRecord::Base
+  attr_accessor :num_data, :num_tree, :num_feature
+
+  validates :num_data, :numericality => {:only_integer => true, :greater_than => 0}
+  validates :num_tree, :numericality => {:only_integer => true, :greater_than => 0}
+  validates :num_feature, :numericality => {:only_integer => true, :greater_than => 0}
+end
