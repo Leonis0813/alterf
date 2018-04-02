65c1d3be05ef6fc61e07d201d7865289796c4f76
Leonis0813 <Leonis.0813@gmail.com>
2018-04-02 12:53:12 +0100

    remove link to data

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,9 +1,4 @@
 ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.default_url_options = {
-  :host => '192.168.33.10',
-  :port => 3000,
-  :script_name => '/alterf',
-}
 ActionMailer::Base.smtp_settings = {
   address: 'smtp.gmail.com',
   domain: 'gmail.com',
