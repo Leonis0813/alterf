86176f67bb1ff562df2e5393868e976284132bcf
Leonis0813 <Leonis.0813@gmail.com>
2018-04-02 05:46:08 +0100

    create controller and error

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   get '/analyses' => 'analyses#manage'
   post '/analyses/learn' => 'analyses#learn'
+
+  namespace :api do
+    resources :analyses, :only => [] do
+      get '/training_data' => 'analyses#download_training_data'
+    end
+  end
 end
