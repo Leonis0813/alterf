a7d4997c4b4b93ce8ec6ab149ce33efda22c9fbb
Leonis0813 <Leonis.0813@gmail.com>
2018-04-02 15:30:04 +0100

    add grade and weight_diff

--- scripts/learn.r
+++ scripts/learn.r
@@ -6,15 +6,16 @@ mtry <- as.integer(args[4])
 
 library(yaml)
 config <- yaml.load_file("scripts/settings.yml")
-
+print(paste(config$features, collapse=","))
 library(RMySQL)
 driver <- dbDriver("MySQL")
-dbconnector <- dbConnect(driver, dbname="alterf", user=config$mysql$user, password=config$mysql$password, host=config$mysql$host, port=as.integer(config$mysql$port))
+dbconnector <- dbConnect(driver, dbname="denebola", user=config$mysql$user, password=config$mysql$password, host=config$mysql$host, port=as.integer(config$mysql$port))
 
-sql <- paste("SELECT age, direction, distance, number, weight, place, round, track, weather, burden_weight, IF(`order` = '1', 1, 0) AS won FROM training_data WHERE `order` REGEXP '[0-9]+' LIMIT", num_training_data)
+sql <- paste("SELECT ", paste(config$features, collapse=","), ", IF(`order` = '1', 1, 0) AS won FROM features WHERE `order` REGEXP '[0-9]+' LIMIT", num_training_data)
 training_data <- dbGetQuery(dbconnector, sql)
 training_data$direction <- as.factor(training_data$direction)
 training_data$place <- as.factor(training_data$place)
+training_data$grade <- as.factor(training_data$grade)
 training_data$track <- as.factor(training_data$track)
 training_data$weather <- as.factor(training_data$weather)
 training_data$won <- as.factor(training_data$won)
@@ -27,6 +28,21 @@ write(paste("num_of_training_data:", num_training_data), file=paste(filename, "y
 write(paste("ntree:", ntree), file=paste(filename, "yml", sep="."), append=T)
 write(paste("mtry:", mtry), file=paste(filename, "yml", sep="."), append=T)
 write("training_data:", file=paste(filename, "yml", sep="."), append=T)
-attributes <- paste(training_data$age, training_data$direction, training_data$distance, training_data$number, training_data$weight, training_data$place, training_data$round, training_data$track, training_data$weather, training_data$burden_weight, training_data$won, sep=", ")
+attributes <- paste(
+  training_data$age,
+  training_data$direction,
+  training_data$distance,
+  training_data$number,
+  training_data$place,
+  training_data$round,
+  training_data$grade,
+  training_data$track,
+  training_data$weather,
+  training_data$burden_weight,
+  training_data$weight,
+  training_data$weight_diff,
+  training_data$won,
+  sep=", "
+)
 write(paste("  - [", attributes, "]", sep=""), file=paste(filename, "yml", sep="."), append=T)
 save(model, file=paste("results/", id, ".rf", sep=""))
