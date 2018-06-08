436e96a7a8e4f86eb3e947259026fcda8125c8b8
Leonis0813 <Leonis.0813@gmail.com>
2018-06-08 14:25:02 +0100

    update controller

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -9,18 +9,27 @@ class PredictionsController < ApplicationController
     absent_keys = prediction_params - attributes.symbolize_keys.keys
     raise BadRequest.new(absent_keys.map {|key| "absent_param_#{key}" }) unless absent_keys.empty?
 
-    invalid_keys = attributes.select do |key, value|
-      not value.respond_to?(:original_filename)
-    end.keys
+    invalid_keys = [].tap do |keys|
+      model = attributes[:model]
+      if model.respond_to?(:original_filename)
+        attributes[:model] = model.original_filename
+      else
+        keys << :model
+      end
+
+      test_data = attributes[:test_data]
+      if test_data.respond_to?(:original_filename)
+        attributes[:test_data] = test_data.original_filename
+      elsif not (test_data.kind_of?(String) and test_data.match(URI::regexp(%w[http https])))
+        keys << :test_data
+      end
+    end
+
     unless invalid_keys.empty?
       raise BadRequest.new(invalid_keys.map {|key| "invalid_param_#{key}" })
     end
 
-    prediction = Prediction.new(
-      :model => attributes[:model].original_filename,
-      :test_data => attributes[:test_data].original_filename,
-      :state => 'processing'
-    )
+    prediction = Prediction.new(attributes.merge(:state => 'processing'))
     if prediction.save
       PredictionJob.perform_later(prediction.id)
       render :status => :ok, :json => {}
