32f46d389d848b2ce538b031bc5567703efe8ec2
Leonis0813 <Leonis.0813@gmail.com>
2018-06-09 12:05:06 +0100

    update request spec

--- spec/requests/manage_prediction_spec.rb
+++ spec/requests/manage_prediction_spec.rb
@@ -16,8 +16,18 @@ describe 'ブラウザで予測する', :type => :request do
     it '予測画面が表示されていること' do
       is_asserted_by { @driver.current_url == "#{base_url}/predictions" }
       is_asserted_by { @driver.find_element(:id, 'prediction_model') }
-      is_asserted_by { @driver.find_element(:id, 'prediction_test_data') }
+      is_asserted_by { @driver.find_element(:id, 'type_file').selected? }
+      is_asserted_by { @driver.find_element(:xpath, '//input[@id="prediction_test_data"][@type="file"]') }
       is_asserted_by { @driver.find_element(:xpath, '//form/input[@value="実行"]') }
+    end
+
+    describe 'テストデータ形式を変更する' do
+      before(:all) { @driver.find_element(:id, 'type_url').click }
+
+      it 'URLを指定できること' do
+        is_asserted_by { @driver.find_element(:id, 'type_url').selected? }
+        is_asserted_by { @driver.find_element(:xpath, '//input[@id="prediction_test_data"][@type="url"]') }
+      end
     end
   end
 end
