c41b5793dc0a64240d00627ab34192bf89f63fd0
Leonis0813 <Leonis.0813@gmail.com>
2018-06-25 11:58:50 +0100

    create tmp dir

--- app/jobs/analysis_job.rb
+++ app/jobs/analysis_job.rb
@@ -4,8 +4,10 @@ class AnalysisJob < ActiveJob::Base
   def perform(analysis_id)
     analysis = Analysis.find(analysis_id)
     args = [analysis_id, analysis.num_data, analysis.num_tree, analysis.num_feature]
+    FileUtils.mkdir_p("#{Rails.root}/tmp/files/#{analysis_id}")
     ret = system "Rscript #{Rails.root}/scripts/analyze.r #{args.join(' ')}"
     analysis.update!(:state => 'completed')
     AnalysisMailer.finished(analysis, ret).deliver_now
+    FileUtils.rm_rf("#{Rails.root}/tmp/files/#{analysis_id}")
   end
 end
