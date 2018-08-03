6db54c4feb1158ebe3dd69dbee979d42c52a4f23
Leonis0813 <Leonis.0813@gmail.com>
2018-08-04 00:30:05 +0100

    modify controller

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -1,7 +1,7 @@
 class PredictionsController < ApplicationController
   def manage
     @prediction = Prediction.new
-    @predictions = Prediction.all.order(:created_at => :desc)
+    @predictions = Prediction.all.order(:created_at => :desc).page(params[:page])
   end
 
   def execute
@@ -29,7 +29,7 @@ class PredictionsController < ApplicationController
       raise BadRequest.new(invalid_keys.map {|key| "invalid_param_#{key}" })
     end
 
-    prediction = Prediction.new(attributes.merge(:state => 'processing'))
+    prediction = Prediction.new(attributes.merge(:state => 'completed'))
     if prediction.save
       params.slice(*prediction_params).values.each do |value|
         if value.respond_to?(:original_filename)
