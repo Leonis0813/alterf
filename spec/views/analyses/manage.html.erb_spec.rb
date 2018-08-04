61073a57fc1206b5ceb9fbc78e471c6c2523a14e
Leonis0813 <Leonis.0813@gmail.com>
2018-08-04 04:35:32 +0100

    update spec for analyses view

--- spec/views/analyses/manage.html.erb_spec.rb
+++ spec/views/analyses/manage.html.erb_spec.rb
@@ -2,13 +2,14 @@
 require 'rails_helper'
 
 describe 'analyses/manage', :type => :view do
+  per_page = 1
   row_xpath = '//div[@id="main-content"]/div[@class="row center-block"]'
 
   shared_context '分析ジョブを登録する' do |num|
     before(:all) do
       param = {:num_data => 10000, :num_tree => 100, :num_feature => 100}
       num.times { Analysis.create!(param.merge(:state => %w[processing completed].sample)) }
-      @analyses = Analysis.order(:created_at => :desc)
+      @analyses = Analysis.order(:created_at => :desc).page(1)
     end
 
     after(:all) { Analysis.destroy_all }
@@ -49,19 +50,57 @@ describe 'analyses/manage', :type => :view do
     end
   end
 
-  shared_examples 'ジョブ実行履歴が表示されていること' do |expected_size|
+  shared_examples 'ジョブ実行履歴が表示されていること' do |expected_size: 0, total: 0, from: 0, to: 0|
     table_panel_xpath = [
       row_xpath,
       'div[@class="col-lg-8"]',
     ].join('/')
 
     it 'タイトルが表示されていること' do
-      expect(@html).to have_selector("#{table_panel_xpath}/h4", :text => 'ジョブ実行履歴')
+      expect(@html).to have_selector("#{table_panel_xpath}/h3", :text => 'ジョブ実行履歴')
+    end
+
+    it '件数情報が表示されていること' do
+      info_xpath = "#{table_panel_xpath}/h4"
+      expect(@html).to have_selector(info_xpath, :text => "#{total}件中#{from}〜#{to}件を表示")
+    end
+
+    paging_xpath = "#{table_panel_xpath}/nav/ul[@class='pagination']"
+
+    it '先頭のページへのボタンが表示されていないこと' do
+      xpath = "#{paging_xpath}/li[@class='pagination']/span[@class='first']/a"
+      expect(@html).not_to have_selector(xpath, :text => I18n.t('views.list.pagination.first'))
+    end
+
+    it '前のページへのボタンが表示されていないこと' do
+      xpath = "#{paging_xpath}/li[@class='pagination']/span[@class='prev']/a"
+      expect(@html).not_to have_selector(xpath, :text => I18n.t('views.list.pagination.previous'))
+    end
+
+    it '1ページ目が表示されていること' do
+      xpath = "#{paging_xpath}/li[@class='page-item active']"
+      expect(@html).to have_selector(xpath, :text => 1)
+    end
+
+    it '2ページ目が表示されていること' do
+      xpath = "#{paging_xpath}/li[@class='page-item']/a[href='/analyses?page=2']"
+      expect(@html).to have_selector(xpath, :text => 2)
+    end
+
+    it '次のページへのボタンが表示されていること' do
+      xpath = "#{paging_xpath}/li[@class='page-item']/span[@class='next']/a[href='/analyses?page=2']"
+      expect(@html).to have_selector(xpath, :text => I18n.t('views.pagination.next'))
+    end
+
+    it '最後のページへのボタンが表示されていること' do
+      xpath = "#{paging_xpath}/li[@class='page-item']/span[@class='last']/a"
+      expect(@html).to have_selector(xpath, :text => I18n.t('views.pagination.last'))
     end
 
     %w[ 実行開始日時 学習データ数 決定木の数 特徴量の数 状態 ].each do |header|
       it "ヘッダー(#{header})があること" do
-        expect(@html).to have_selector("#{table_panel_xpath}/table[@class='table table-hover']/thead/th", :text => header)
+        xpath = "#{table_panel_xpath}/table[@class='table table-hover']/thead/th"
+        expect(@html).to have_selector(xpath, :text => header)
       end
     end
 
@@ -81,7 +120,10 @@ describe 'analyses/manage', :type => :view do
     end
   end
 
-  before(:all) { @analysis = Analysis.new }
+  before(:all) do
+    Kaminari.config.default_per_page = per_page
+    @analysis = Analysis.new
+  end
 
   before(:each) do
     render :template => 'analyses/manage', :layout => 'layouts/application'
@@ -92,6 +134,11 @@ describe 'analyses/manage', :type => :view do
     include_context '分析ジョブを登録する', 10
     it_behaves_like 'ヘッダーが表示されていること'
     it_behaves_like '入力フォームが表示されていること'
-    it_behaves_like 'ジョブ実行履歴が表示されていること', 10
+    it_behaves_like 'ジョブ実行履歴が表示されていること', {
+                      :expected_size => 1,
+                      :total => 10,
+                      :from => 1,
+                      :to => 1,
+                    }
   end
 end
