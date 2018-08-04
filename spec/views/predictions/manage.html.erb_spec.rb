2ce1c3c2f2bb1526f38a1e6c3682261735b49766
Leonis0813 <Leonis.0813@gmail.com>
2018-08-04 04:39:06 +0100

    update spec for predictions view

--- spec/views/predictions/manage.html.erb_spec.rb
+++ spec/views/predictions/manage.html.erb_spec.rb
@@ -2,13 +2,14 @@
 require 'rails_helper'
 
 describe 'predictions/manage', :type => :view do
+  per_page = 1
   row_xpath = '//div[@id="main-content"]/div[@class="row center-block"]'
 
   shared_context '予測ジョブを登録する' do |num|
     before(:all) do
       param = {:model => 'model', :test_data => 'test_data'}
       num.times { Prediction.create!(param.merge(:state => %w[processing completed].sample)) }
-      @predictions = Prediction.order(:created_at => :desc)
+      @predictions = Prediction.order(:created_at => :desc).page(1)
     end
 
     after(:all) { Prediction.destroy_all }
@@ -58,7 +59,7 @@ describe 'predictions/manage', :type => :view do
     end
   end
 
-  shared_examples 'ジョブ実行履歴が表示されていること' do |expected_size|
+  shared_examples 'ジョブ実行履歴が表示されていること' do |expected_size: 0, total: 0, from: 0, to: 0|
     table_panel_xpath = [
       row_xpath,
       'div[@class="col-lg-8"]',
@@ -66,6 +67,43 @@ describe 'predictions/manage', :type => :view do
 
     it 'タイトルが表示されていること' do
       expect(@html).to have_selector("#{table_panel_xpath}/h4", :text => 'ジョブ実行履歴')
+    end
+
+    it '件数情報が表示されていること' do
+      info_xpath = "#{table_panel_xpath}/h4"
+      expect(@html).to have_selector(info_xpath, :text => "#{total}件中#{from}〜#{to}件を表示")
+    end
+
+    paging_xpath = "#{table_panel_xpath}/nav/ul[@class='pagination']"
+
+    it '先頭のページへのボタンが表示されていないこと' do
+      xpath = "#{paging_xpath}/li[@class='pagination']/span[@class='first']/a"
+      expect(@html).not_to have_selector(xpath, :text => I18n.t('views.list.pagination.first'))
+    end
+
+    it '前のページへのボタンが表示されていないこと' do
+      xpath = "#{paging_xpath}/li[@class='pagination']/span[@class='prev']/a"
+      expect(@html).not_to have_selector(xpath, :text => I18n.t('views.list.pagination.previous'))
+    end
+
+    it '1ページ目が表示されていること' do
+      xpath = "#{paging_xpath}/li[@class='page-item active']"
+      expect(@html).to have_selector(xpath, :text => 1)
+    end
+
+    it '2ページ目が表示されていること' do
+      xpath = "#{paging_xpath}/li[@class='page-item']/a[href='/predictions?page=2']"
+      expect(@html).to have_selector(xpath, :text => 2)
+    end
+
+    it '次のページへのボタンが表示されていること' do
+      xpath = "#{paging_xpath}/li[@class='page-item']/span[@class='next']/a[href='/predictions?page=2']"
+      expect(@html).to have_selector(xpath, :text => I18n.t('views.pagination.next'))
+    end
+
+    it '最後のページへのボタンが表示されていること' do
+      xpath = "#{paging_xpath}/li[@class='page-item']/span[@class='last']/a"
+      expect(@html).to have_selector(xpath, :text => I18n.t('views.pagination.last'))
     end
 
     %w[ 実行開始日時 モデル テストデータ 状態 ].each do |header|
@@ -90,7 +128,10 @@ describe 'predictions/manage', :type => :view do
     end
   end
 
-  before(:all) { @prediction = Prediction.new }
+  before(:all) do
+    Kaminari.config.default_per_page = per_page
+    @prediction = Prediction.new
+  end
 
   before(:each) do
     render :template => 'predictions/manage', :layout => 'layouts/application'
@@ -101,6 +142,11 @@ describe 'predictions/manage', :type => :view do
     include_context '予測ジョブを登録する', 10
     it_behaves_like 'ヘッダーが表示されていること'
     it_behaves_like '入力フォームが表示されていること'
-    it_behaves_like 'ジョブ実行履歴が表示されていること', 10
+    it_behaves_like 'ジョブ実行履歴が表示されていること', {
+                      :expected_size => 1,
+                      :total => 10,
+                      :from => 1,
+                      :to => 1,
+                    }
   end
 end
