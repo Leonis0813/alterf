23882f13e35eedcf0045b839f17f2b0c75fb27e9
Leonis0813 <Leonis.0813@gmail.com>
2018-11-15 16:28:42 +0000

    create zip file in analysis_mailer

--- app/mailers/analysis_mailer.rb
+++ app/mailers/analysis_mailer.rb
@@ -6,8 +6,18 @@ class AnalysisMailer < ApplicationMailer
     @analysis = analysis
     subject = is_success ? '分析が完了しました' : '分析中にエラーが発生しました'
     template_name = is_success ? 'success' : 'failer'
-    attachments['training_data.yml'] = File.read(File.join(Rails.root, "tmp/files/#{analysis.id}/analysis.yml"))
-    attachments['model.rf'] = File.read(File.join(Rails.root, "tmp/files/#{analysis.id}/model.rf"))
+    tmp_dir = File.join(Rails.root, "tmp/files/#{analysis.id}")
+
+    file_names = %w[ training_data.yml model.rf ]
+    zip_file_name = File.join(tmp_dir, 'analysis.zip')
+
+    Zip::File.open(zip_file_name, Zip::File::CREATE) do |zip|
+      file_names.each do |file_name|
+        zip.add(file_name, File.join(tmp_dir, filename))
+      end
+    end
+
+    attachments['analysis.zip'] = File.read(File.join(tmp_dir, 'analysis.zip'))
     mail(:to => 'Leonis.0813@gmail.com', :subject => subject, :template_name => template_name)
   end
 end
