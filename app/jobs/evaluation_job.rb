c83540efd2d6473215da673a8b4d4dc6619c729d
Leonis0813 <Leonis.0813@gmail.com>
2019-04-19 13:59:54 +0000

    complete evaluation job and refactor

--- app/jobs/evaluation_job.rb
+++ app/jobs/evaluation_job.rb
@@ -1,12 +1,22 @@
-# coding: utf-8
 class EvaluationJob < ActiveJob::Base
   queue_as :alterf
 
   def perform(evaluation_id)
     evaluation = Evaluation.find(evaluation_id)
-    data_dir = "#{Rails.root}/tmp/files/#{evaluation_id}"
-    args = [evaluation_id, evaluation.model]
-    ret = system "Rscript #{Rails.root}/scripts/evaluate.r #{args.join(' ')}"
+    data_dir = File.join(Rails.root, 'tmp/files', evaluation_id)
+
+    client = NetkeibaClient.new
+
+    client.get_race_top.each do |race_id|
+      race = client.get_race("#{Settings.netkeiba.base_url}/race/#{race_id}")
+      file = File.open("#{data_dir}/#{Settings.prediction.tmp_file_name}", 'w')
+      YAML.dump(race, file)
+
+      args = [evaluation_id, evaluation.model, Settings.evaluation.tmp_file_name]
+      ret = system "Rscript #{Rails.root}/scripts/predict.r #{args.join(' ')}"
+      FileUtils.mv("#{data_dir}/prediction.yml", "#{data_dir}/#{race_id}.yml")
+    end
+
     evaluation.update!(:state => 'completed')
     EvaluationMailer.finished(evaluation, ret).deliver_now
     FileUtils.rm_rf(data_dir)
--- app/jobs/prediction_job.rb
+++ app/jobs/prediction_job.rb
@@ -1,81 +1,27 @@
-# coding: utf-8
 class PredictionJob < ActiveJob::Base
   queue_as :alterf
-
-  TEST_DATA_FILE_NAME = 'test_data.yml'
 
   def perform(prediction_id)
     prediction = Prediction.find(prediction_id)
     data_dir = "#{Rails.root}/tmp/files/#{prediction_id}"
     test_data = prediction.test_data
-    if prediction.test_data.match(URI::regexp)
+
+    if test_data.match(URI::regexp)
       begin
-        generate_test_data(prediction.test_data, "#{data_dir}/#{TEST_DATA_FILE_NAME}")
+        race = NetkeibaClient.new.get_race(test_data)
+        file = File.open("#{data_dir}/#{Settings.prediction.tmp_file_name}", 'w')
+        YAML.dump(race, file)
       rescue Exception => e
         PredictionMailer.finished(prediction, false).deliver_now
         raise e
       end
-      test_data = TEST_DATA_FILE_NAME
     end
-    args = [prediction_id, prediction.model, test_data]
+
+    args = [prediction_id, prediction.model, Settings.prediction.tmp_file_name]
     ret = system "Rscript #{Rails.root}/scripts/predict.r #{args.join(' ')}"
+
     prediction.update!(:state => 'completed')
     PredictionMailer.finished(prediction, ret).deliver_now
     FileUtils.rm_rf(data_dir)
-  end
-
-  private
-
-  def generate_test_data(url, output_path)
-    parsed_url = URI.parse(url)
-    res = Net::HTTP.start(parsed_url.host, parsed_url.port, :use_ssl => true) do |http|
-      http.request Net::HTTP::Get.new(parsed_url)
-    end
-    html = Nokogiri::HTML.parse(res.body.encode('UTF-8', 'EUC-JP').gsub('&nbsp;', ' '))
-    feature = extract_feature(html)
-
-    File.open(output_path, 'w') do |f|
-      f.puts "direction: #{feature[:direction]}"
-      f.puts "distance: #{feature[:distance]}"
-      f.puts "grade: #{feature[:grade]}"
-      f.puts "place: #{feature[:place]}"
-      f.puts "round: #{feature[:round]}"
-      f.puts "track: #{feature[:track]}"
-      f.puts "weather: #{feature[:weather]}"
-
-      f.puts 'test_data:'
-      feature[:test_data].each do |test_data|
-        f.puts "  - #{test_data}"
-      end
-    end
-  end
-
-  def extract_feature(html)
-    {}.tap do |feature|
-      race_data = html.xpath('//dl[contains(@class, "racedata")]')
-
-      track, weather, _ = race_data.search('span').text.split('/')
-      feature[:track] = track[0].sub('ダ', 'ダート')
-      feature[:direction] = track[1]
-      feature[:distance] = track.match(/(\d*)m/)[1].to_i
-      feature[:weather] = weather.match(/天候 : (.*)/)[1]
-      feature[:grade] = race_data.search('h1').text.match(/\((.*)\)$/).try(:[], 1)
-      feature[:place] = html.xpath('//ul[contains(@class, "race_place")]').first
-                        .children[1].text.match(/<a.*class="active">(.*?)<\/a>/)[1]
-      feature[:round] = race_data.search('dt').text.strip.match(/^(\d*) R$/)[1].to_i
-
-      _, *data = race_data.xpath('//table[contains(@class, "race_table")]').search('tr')
-      feature[:test_data] = data.map do |entry|
-        attributes = entry.search('td').map(&:text).map(&:strip)
-
-        [
-          attributes[4].match(/(\d+)\z/)[1].to_i,
-          attributes[5].to_f,
-          attributes[2].to_i,
-          attributes[14].match(/\A(\d+)/).try(:[], 1).to_f,
-          attributes[14].match(/\((.+)\)$/).try(:[], 1).to_f,
-        ]
-      end
-    end
   end
 end
