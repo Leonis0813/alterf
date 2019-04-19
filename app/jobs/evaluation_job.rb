771f20387f1ea99a61cca0d4788110d88668d4fe
Leonis0813 <Leonis.0813@gmail.com>
2019-04-19 17:38:38 +0000

    fix bug for evaluation

--- app/jobs/evaluation_job.rb
+++ app/jobs/evaluation_job.rb
@@ -5,23 +5,23 @@ class EvaluationJob < ActiveJob::Base
 
   def perform(evaluation_id)
     evaluation = Evaluation.find(evaluation_id)
-    data_dir = File.join(Rails.root, 'tmp/files', evaluation_id)
+    data_dir = File.join(Rails.root, 'tmp/files', evaluation_id.to_s)
 
     client = NetkeibaClient.new
 
     client.get_race_top.each do |race_id|
       race = client.get_race("#{Settings.netkeiba.base_url}/race/#{race_id}")
-      File.open("#{data_dir}/#{Settings.prediction.tmp_file_name}", 'w') do
+      File.open("#{data_dir}/#{Settings.prediction.tmp_file_name}", 'w') do |file|
         YAML.dump(race.stringify_keys, file)
       end
 
       args = [evaluation_id, evaluation.model, Settings.evaluation.tmp_file_name]
-      ret = system "Rscript #{Rails.root}/scripts/predict.r #{args.join(' ')}"
+      system "Rscript #{Rails.root}/scripts/predict.r #{args.join(' ')}"
       FileUtils.mv("#{data_dir}/prediction.yml", "#{data_dir}/#{race_id}.yml")
     end
 
     evaluation.update!(:state => 'completed')
-    EvaluationMailer.finished(evaluation, ret).deliver_now
+    EvaluationMailer.finished(evaluation, true).deliver_now
     FileUtils.rm_rf(data_dir)
   end
 end
