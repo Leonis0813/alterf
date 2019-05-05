2edd0d065676e0f916791233f58a487bcb073a0d
Leonis0813 <Leonis.0813@gmail.com>
2019-05-05 05:32:30 +0000

    fix Metrics/AbcSize

--- lib/clients/netkeiba_client.rb
+++ lib/clients/netkeiba_client.rb
@@ -15,14 +15,16 @@ class NetkeibaClient < HTTPClient
       race_data = html.xpath('//dl[contains(@class, "racedata")]')
 
       track, weather, = race_data.search('span').text.split('/')
-      feature[:track] = track[0].sub('ダ', 'ダート')
-      feature[:direction] = track[1]
-      feature[:distance] = track.match(/(\d*)m/)[1].to_i
-      feature[:weather] = weather.match(/天候 : (.*)/)[1].strip
-      feature[:grade] = race_data.search('h1').text.match(/\((.*)\)$/).try(:[], 1)
-      feature[:place] = html.xpath('//ul[contains(@class, "race_place")]').first
-                            .children[1].text.match(%r{<a.*class="active">(.*?)</a>})[1]
-      feature[:round] = race_data.search('dt').text.strip.match(/^(\d*) R$/)[1].to_i
+      feature.merge!{
+        track: track[0].sub('ダ', 'ダート'),
+        direction: track[1],
+        distance: track.match(/(\d*)m/)[1].to_i,
+        weather: weather.match(/天候 : (.*)/)[1].strip,
+        grade: race_data.search('h1').text.match(/\((.*)\)$/).try(:[], 1),
+        place: html.xpath('//ul[contains(@class, "race_place")]').first
+                   .children[1].text.match(%r{<a.*class="active">(.*?)</a>})[1],
+        round: race_data.search('dt').text.strip.match(/^(\d*) R$/)[1].to_i,
+      }
 
       _, *data = race_data.xpath('//table[contains(@class, "race_table")]').search('tr')
       feature[:test_data] = data.map do |entry|
