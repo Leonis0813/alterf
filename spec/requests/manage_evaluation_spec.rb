7e5a62c1e4f3e6656293e279fc858cd67207f47f
Leonis0813 <Leonis.0813@gmail.com>
2019-06-19 13:20:12 +0000

    test error case

--- spec/requests/manage_evaluation_spec.rb
+++ spec/requests/manage_evaluation_spec.rb
@@ -13,5 +13,24 @@ describe 'ブラウザで予測する', type: :request do
       is_asserted_by { @driver.find_element(:id, 'evaluation_model') }
       is_asserted_by { @driver.find_element(:xpath, '//form/input[@value="実行"]') }
     end
+
+    describe 'モデルを指定せずに実行する' do
+      before(:all) do
+        @driver.find_element(:xpath, '//form/input[@value="実行"]').click
+        @wait.until { @driver.find_element(:class, 'modal-body').displayed? }
+      end
+
+      it 'タイトルが正しいこと' do
+        xpath = '//div[@class="modal-header"]/h4[@class="modal-title"]'
+        text = 'エラーが発生しました'
+        is_asserted_by { @driver.find_element(:xpath, xpath).text == text }
+      end
+
+      it 'エラーメッセージが正しいこと' do
+        xpath = '//div[@class="modal-body"]/div'
+        text = '入力値を見直してください'
+        is_asserted_by { @driver.find_element(:xpath, xpath).text == text }
+      end
+    end
   end
 end
--- spec/requests/manage_prediction_spec.rb
+++ spec/requests/manage_prediction_spec.rb
@@ -18,14 +18,35 @@ describe 'ブラウザで予測する', type: :request do
       is_asserted_by { @driver.find_element(:xpath, '//form/input[@value="実行"]') }
     end
 
-    describe 'テストデータ形式を変更する' do
-      before(:all) { @driver.find_element(:id, 'type_url').click }
+    describe '不正な値を入力する' do
+      before(:all) do
+        @driver.find_element(:id, 'type_url').click
+        @driver.find_element(:id, 'prediction_test_data').send_keys('invalid')
+        @driver.find_element(:xpath, '//form/input[@value="実行"]').click
+        @wait.until { @driver.find_element(:class, 'modal-body').displayed? }
+      end
 
-      it 'URLを指定できること' do
-        is_asserted_by { @driver.find_element(:id, 'type_url').selected? }
+      it 'タイトルが正しいこと' do
+        xpath = '//div[@class="modal-header"]/h4[@class="modal-title"]'
+        text = 'エラーが発生しました'
+        is_asserted_by { @driver.find_element(:xpath, xpath).text == text }
+      end
 
-        xpath = '//input[@id="prediction_test_data"][@type="url"]'
-        is_asserted_by { @driver.find_element(:xpath, xpath) }
+      it 'エラーメッセージが正しいこと' do
+        xpath = '//div[@class="modal-body"]/div'
+        text = '入力値を見直してください'
+        is_asserted_by { @driver.find_element(:xpath, xpath).text == text }
+      end
+
+      describe 'テストデータ形式を変更する' do
+        before(:all) { @driver.find_element(:id, 'type_url').click }
+
+        it 'URLを指定できること' do
+          is_asserted_by { @driver.find_element(:id, 'type_url').selected? }
+
+          xpath = '//input[@id="prediction_test_data"][@type="url"]'
+          is_asserted_by { @driver.find_element(:xpath, xpath) }
+        end
       end
     end
   end
