d105a6e98153a4b0eb1677c290ba74e1be07ebe2
Leonis0813 <Leonis.0813@gmail.com>
2019-07-20 10:36:13 +0000

    add model for denebola

---
+++ app/models/denebola.rb
@@ -0,0 +1,5 @@
+class Denebola < ActiveRecord::Base
+  file_path = Rails.root.join('config', 'denebola', 'database.yml')
+  establish_connection(YAML.load_file(file_path)[Rails.env])
+  self.abstract_class = true
+end
