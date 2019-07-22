c3fba68a5df22833f92d2811088cab04959efd51
Leonis0813 <Leonis.0813@gmail.com>
2019-07-22 03:48:42 +0000

    fix client

--- lib/clients/netkeiba_client.rb
+++ lib/clients/netkeiba_client.rb
@@ -6,8 +6,8 @@ class NetkeibaClient < HTTPClient
     res.body.scan(%r{.*/race/(\d+)}).flatten
   end
 
-  def http_get_race_name(path)
-    url = Settings.netkeiba.base_url + path
+  def http_get_race_name(race_id)
+    url = "#{Settings.netkeiba.base_url}/race/#{race_id}"
     html =
       Nokogiri::HTML.parse(get(url).body.encode('UTF-8', 'EUC-JP').gsub('&nbsp;', ' '))
     html.xpath('//dl[contains(@class, "racedata")]/dd/h1').text.strip
