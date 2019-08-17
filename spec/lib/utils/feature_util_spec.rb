d022d04bd7e04e7badda7d853645d992c6e1389f
Leonis0813 <Leonis.0813@gmail.com>
2019-08-17 06:48:10 +0000

    refactor

--- spec/lib/utils/feature_util_spec.rb
+++ spec/lib/utils/feature_util_spec.rb
@@ -4,52 +4,18 @@ require 'rails_helper'
 
 describe FeatureUtil do
   describe '.create_feature_from_denebola' do
-    feature = {
-      direction: '左',
-      distance: 1800,
-      grade: 'G1',
-      month: 5,
-      place: '中京',
-      round: 11,
-      track: '芝',
-      weather: '晴',
-      entries: [
-        {
-          age: 3,
-          blank: 7,
-          burden_weight: 56,
-          distance_diff: 0.123,
-          entry_times: 3,
-          horse_average_prize_money: 100,
-          last_race_order: 10,
-          number: 5,
-          rate_within_third: 0.33,
-          running_style: '逃げ',
-          second_last_race_order: 2,
-          sex: '牝',
-          weight: 402,
-          weight_diff: -8,
-          weight_per: 0.139303,
-          win_times: 1,
-          won: true,
-          jockey_average_prize_money: 1000,
-          jockey_win_rate: 0.3,
-          jockey_win_rate_last_four_races: 0.2,
-        },
-      ],
-    }
-
-    horse_id = '2017103903'
+    dirs = %w[spec fixtures lib utils feature_util create_feature_from_denebola]
+    file_path = Rails.root.join(dirs.join('/'), 'feature.yml')
+    feature = YAML.load_file(file_path).with_indifferent_access
+    entries = feature[:entries].map(&:values)
+    expected = feature.except(:entries).merge(entries: entries).stringify_keys
     race_id = '201809030811'
-    expected = feature.except(:entries).merge(
-      entries: feature[:entries].map(&:values),
-    ).stringify_keys
 
     describe '正常系' do
       before(:all) do
         feature[:entries].map do |entry|
           attribute = feature.except(:entries).merge(entry).merge(
-            horse_id: horse_id,
+            horse_id: '2017103903',
             race_id: race_id,
           )
           Denebola::Feature.create!(attribute)
@@ -66,64 +32,11 @@ describe FeatureUtil do
   end
 
   describe '.create_feature_from_netkeiba' do
-    race = {
-      direction: '左',
-      distance: 1800,
-      grade: 'G1',
-      month: 5,
-      place: '中京',
-      round: 11,
-      track: '芝',
-      weather: '晴',
-      entries: [
-        {
-          age: 3,
-          burden_weight: 56,
-          order: 2,
-          number: 5,
-          sex: '牝',
-          weight: 402,
-          weight_diff: -8,
-          weight_per: 0.139303,
-          horse_link: '/horse/2013106099',
-        },
-      ],
-    }
-
-    horse = {
-      running_style: '逃げ',
-      results: [
-        {
-          race_id: '201809030811',
-          date: Date.parse('2018/12/23'),
-          order: 3,
-          distance: 1800,
-          prize_money: 45000000,
-        },
-        {
-          race_id: '201809030810',
-          date: Date.parse('2018/12/16'),
-          order: 5,
-          distance: 1400,
-          prize_money: 0,
-        },
-      ],
-    }
-
-    jockey = {
-      results: [
-        {
-          race_id: '201809030811',
-          order: 3,
-          prize_money: 45000000,
-        },
-        {
-          race_id: '201809030810',
-          order: 5,
-          prize_money: 0,
-        },
-      ],
-    }
+    dirs =
+      %w[spec fixtures lib utils feature_util create_feature_from_netkeiba].join('/')
+    race = YAML.load_file(Rails.root.join(dirs, 'race.yml')).with_indifferent_access
+    horse = YAML.load_file(Rails.root.join(dirs, 'horse.yml')).with_indifferent_access
+    jockey = YAML.load_file(Rails.root.join(dirs, 'jockey.yml')).with_indifferent_access
 
     expected = race.except(:entries).merge(
       entries: [
