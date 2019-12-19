9f65fc4fb4f17f35e866e54c4d83d58b04939a09
Leonis0813 <Leonis.0813@gmail.com>
2019-12-19 11:13:01 +0000

    add analysis_id validation

--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -1,6 +1,8 @@
 class Analysis < ApplicationRecord
-  validates :num_data, :num_tree, :state,
+  validates :analysis_id, :num_data, :num_tree, :state,
             presence: {message: 'absent'}
+  validates :analysis_id,
+            format: {with: /\A[0-9a-f]{32}\z/, message: 'invalid'}
   validates :num_data, :num_tree,
             numericality: {only_integer: true, greater_than: 0, message: 'invalid'},
             allow_nil: true
