9b0a32fa4287ed4a36aaba742b0884e1ed32abaa
Leonis0813 <Leonis.0813@gmail.com>
2019-12-22 02:49:54 +0000

    set num_data in controller

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -5,14 +5,15 @@ class EvaluationsController < ApplicationController
   end
 
   def execute
-    check_absent_param(evaluation_param, %i[model data_source])
+    check_absent_param(execute_param, %i[model data_source])
     check_invalid_param
 
-    model = evaluation_param[:model]
+    model = execute_param[:model]
     evaluation = Evaluation.new(
       evaluation_id: SecureRandom.hex,
       model: model.original_filename,
-      data_source: evaluation_param[:data_source],
+      data_source: execute_param[:data_source],
+      num_data: num_data,
       state: 'processing',
     )
     unless evaluation.save
@@ -26,7 +27,7 @@ class EvaluationsController < ApplicationController
       f.write(model.read)
     end
 
-    if %w[file text].include?(evaluation.data_source)
+    if user_specified_data?
       file_path = File.join(output_dir, Settings.evaluation.race_list_filename)
       File.open(file_path, 'w') do |f|
         race_ids.each {|race_id| f.puts(race_id) }
@@ -45,25 +46,46 @@ class EvaluationsController < ApplicationController
   private
 
   def check_invalid_param
-    model = evaluation_param[:model]
+    model = execute_param[:model]
     raise BadRequest, 'invalid_param_model' unless model.respond_to?(:original_filename)
 
-    return unless %w[file text].include?(evaluation_param[:data_source])
+    return unless user_specified_data?
 
     raise BadRequest, 'invalid_param_data' if race_ids.empty?
     raise BadRequest, 'invalid_param_data' if race_ids.any?(&:empty?)
   end
 
+  def user_specified_data?
+    data_source = execute_param[:data_source]
+    [Evaluation::DATA_SOURCE_FILE, Evaluation::DATA_SOURCE_TEXT].include?(data_source)
+  end
+
+  def num_data
+    case execute_param[:data_source]
+    when Evaluation::DATA_SOURCE_FILE, Evaluation::DATA_SOURCE_TEXT
+      race_ids.size
+    when Evaluation::DATA_SOURCE_RANDOM
+      execute_param[:num_data]
+    when Evaluation::DATA_SOURCE_REMOTE
+      Evaluation::NUM_DATA_REMOTE
+    end
+  end
+
   def race_ids
-    @race_ids ||= case evaluation_param[:data_source]
+    @race_ids ||= case execute_param[:data_source]
                   when 'file'
-                    evaluation_param[:data].read.lines.map(&:chomp)
+                    execute_param[:data].read.lines.map(&:chomp)
                   when 'text'
-                    evaluation_param[:data].lines.map(&:chomp)
+                    execute_param[:data].lines.map(&:chomp)
                   end
   end
 
-  def evaluation_param
-    @evaluation_param ||= params.permit(:model, :data_source, :data)
+  def execute_param
+    @execute_param ||= request.request_parameters.slice(
+      :model,
+      :data_source,
+      :num_data,
+      :data,
+    )
   end
 end
