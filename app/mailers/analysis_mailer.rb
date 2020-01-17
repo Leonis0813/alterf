c5566388a8aa9d94d4eb33b0f30a5b19269d3f77
Leonis0813 <Leonis.0813@gmail.com>
2020-01-17 12:47:17 +0000

    create multiple zip files

--- app/mailers/analysis_mailer.rb
+++ app/mailers/analysis_mailer.rb
@@ -6,18 +6,23 @@ class AnalysisMailer < ApplicationMailer
 
     tmp_dir = Rails.root.join('tmp', 'files', analysis.id.to_s)
 
-    file_names = %w[metadata.yml model.rf feature.csv training_data.csv]
-    zip_file_name = File.join(tmp_dir, 'analysis.zip')
+    [
+      [%w[metadata.yml model.rf], 'model.zip'],
+      [%w[tree.yml feature.csv training_data.csv], 'analysis.zip'],
+    ].each do |file_names, zip_file_name|
+      zip_file_path = File.join(tmp_dir, 'analysis.zip')
 
-    Zip::File.open(zip_file_name, Zip::File::CREATE) do |zip|
-      file_names.each do |file_name|
-        Dir[File.join(tmp_dir, file_name)].each do |file_path|
-          zip.add(File.basename(file_path), file_path)
+      Zip::File.open(zip_file_path, Zip::File::CREATE) do |zip|
+        file_names.each do |file_name|
+          Dir[File.join(tmp_dir, file_name)].each do |file_path|
+            zip.add(File.basename(file_path), file_path)
+          end
         end
       end
+
+      attachments[zip_file_name] = File.read(zip_file_name)
     end
 
-    attachments['analysis.zip'] = File.read(zip_file_name)
     mail(
       to: 'Leonis.0813@gmail.com',
       subject: '分析が完了しました',
