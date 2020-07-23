144816c9dac11b65752148f98309bef32da26f36
Leonis0813 <Leonis.0813@gmail.com>
2020-07-23 10:42:02 +0000

    fix style

--- app/controllers/api/analyses_controller.rb
+++ app/controllers/api/analyses_controller.rb
@@ -1,18 +1,20 @@
-class Api::AnalysesController < ApplicationController
-  before_action :check_request_analysis, only: %i[show]
+module Api
+  class AnalysesController < ApplicationController
+    before_action :check_request_analysis, only: %i[show]
 
-  def show
-    @analysis = request_analysis
-    render status: :ok
-  end
+    def show
+      @analysis = request_analysis
+      render status: :ok
+    end
 
-  private
+    private
 
-  def check_request_analysis
-    raise NotFound unless request_analysis
-  end
+    def check_request_analysis
+      raise NotFound unless request_analysis
+    end
 
-  def request_analysis
-    @request_analysis ||= Analysis.find_by(request.path_parameters.slice(:analysis_id))
+    def request_analysis
+      @request_analysis ||= Analysis.find_by(request.path_parameters.slice(:analysis_id))
+    end
   end
 end
