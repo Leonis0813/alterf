4d9b18d33f6b8a6c63c7213a9201ad4fc73bbdd5
Leonis0813 <Leonis.0813@gmail.com>
2020-08-11 11:58:08 +0000

    create check_schema method

--- lib/errors/bad_request.rb
+++ lib/errors/bad_request.rb
@@ -1,9 +1,12 @@
 class BadRequest < StandardError
   attr_accessor :errors
 
-  def initialize(error_codes)
-    @errors = Array.wrap(error_codes).map do |error_code|
-      {error_code: error_code}
-    end
+  def initialize(messages: {}, resource: nil)
+    @errors = messages.map do |parameter, error_codes|
+      error_codes.map do |error_code|
+        {error_code: error_code, parameter: parameter, resource: resource}
+      end
+    end.flatten
+    @errors.sort_by! {|error| [error[:error_code], error[:parameter]] }
   end
 end
