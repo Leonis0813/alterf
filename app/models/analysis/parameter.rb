0df6024e577706a2240cb3abcda3f7aeb2a97c21
Leonis0813 <Leonis.0813@gmail.com>
2020-11-23 03:59:29 +0000

    create analysis parameter spec

--- app/models/analysis/parameter.rb
+++ app/models/analysis/parameter.rb
@@ -1,20 +1,16 @@
 class Analysis
   class Parameter < ApplicationRecord
     MAX_FEATURES_LIST = %w[all sqrt log2].freeze
-
-    validates :num_tree, :min_samples_split, :min_samples_leaf, :max_features,
-              presence: {message: MESSAGE_ABSENT}
-    validates :num_tree, :min_samples_split, :min_samples_leaf,
-              numericality: {
-                only_integer: true,
-                greater_than: 0,
-                message: MESSAGE_INVALID,
-              },
-              allow_nil: true
-    validates :max_features,
-              inclusion: {in: MAX_FEATURES_LIST, message: MESSAGE_INVALID},
-              allow_nil: true
+    DEFAULT_MIN_SAMPLES_LEAF = 1.freeze
+    DEFAULT_MIN_SAMPLES_SPLIT = 2.freeze
+    DEFAULT_NUM_TREE = 100.freeze
 
     belongs_to :analysis
+
+    after_initialize if: :new_record? do |parameter|
+      parameter.min_samples_leaf ||= DEFAULT_MIN_SAMPLES_LEAF
+      parameter.min_samples_split ||= DEFAULT_MIN_SAMPLES_SPLIT
+      parameter.num_tree ||= DEFAULT_NUM_TREE
+    end
   end
 end
--- spec/factories/analysis.rb
+++ spec/factories/analysis.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :analysis do
     analysis_id { '0' * 32 }
     num_data { 10000 }
-    num_tree { 100 }
     num_feature { 20 }
     state { 'waiting' }
     result { build(:analysis_result) }
