cc5f434f59ce84748fa1d267639192b54c1c65e3
Leonis0813 <Leonis.0813@gmail.com>
2020-12-06 10:31:35 +0000

    add validation

--- app/models/analysis/result/decision_tree.rb
+++ app/models/analysis/result/decision_tree.rb
@@ -1,6 +1,18 @@
 class Analysis
   class Result
     class DecisionTree < ApplicationRecord
+      validates :tree_id,
+                presence: {message: MESSAGE_ABSENT}
+      validates :tree_id,
+                numericality: {
+                  only_integer: true,
+                  greater_than_or_equal_to: 0,
+                  message: MESSAGE_INVALID,
+                },
+                allow_nil: true
+      validates :tree_id,
+                uniqueness: {scope: 'analysis_result_id', message: MESSAGE_DUPLICATED}
+
       belongs_to :result
       has_many :nodes,
                foreign_key: 'analysis_result_decision_tree_id',
--- app/models/analysis/result/decision_tree/node.rb
+++ app/models/analysis/result/decision_tree/node.rb
@@ -2,6 +2,33 @@ class Analysis
   class Result
     class DecisionTree
       class Node < ApplicationRecord
+        NODE_TYPE_LIST = %w[root split leaf].freeze
+        GROUP_LIST = %w[less greater].freeze
+
+        validates :node_id, :node_type, :feature_name, :threshold,
+                  presence: {message: MESSAGE_ABSENT}
+        validates :node_id,
+                  numericality: {
+                    only_integer: true,
+                    greater_than_or_equal_to: 0,
+                    message: MESSAGE_INVALID,
+                  },
+                  allow_nil: true
+        validates :node_id,
+                  uniqueness: {
+                    scope: 'analysis_result_decision_tree_id',
+                    message: MESSAGE_DUPLICATED,
+                  }
+        validates :node_type,
+                  inclusion: {in: NODE_TYPE_LIST, message: MESSAGE_INVALID}
+                  allow_nil: true
+        validates :group,
+                  inclusion: {in: GROUP_LIST, message: MESSAGE_INVALID},
+                  allow_nil: true
+        validates :threshold,
+                  numericality: {message: MESSAGE_INVALID},
+                  allow_nil: true
+
         belongs_to :decision_tree
         belongs_to :parent,
                    class_name: 'Analysis::Result::DecisionTree::Node',
