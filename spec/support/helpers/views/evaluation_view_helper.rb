a80dfae2c8302ae628386ff8f5bb43945902ee4a
Leonis0813 <Leonis.0813@gmail.com>
2021-05-08 03:32:34 +0000

    fix view spec

--- spec/support/helpers/views/evaluation_view_helper.rb
+++ spec/support/helpers/views/evaluation_view_helper.rb
@@ -1,18 +1,55 @@
+# -*- coding: utf-8 -*-
+
 require_relative 'common_view_helper'
 
 module EvaluationViewHelper
   include CommonViewHelper
 
+  def data_source_map
+    {
+      'remote' => 'Top20',
+      'file' => 'ファイル',
+      'text' => '直接入力',
+      'random' => 'ランダム',
+    }
+  end
+
+  def container_xpath
+    '//div[@id="main-content"][@class="container"]'
+  end
+
+  def form_tab_xpath
+    [container_xpath, 'ul[@class="nav nav-tabs"]'].join('/')
+  end
+
   def form_panel_xpath
-    [row_xpath, 'div[@class="col-lg-4"]', 'div[@id="new-evaluation"]'].join('/')
+    [
+      container_xpath,
+      'div[@class="panel panel-default"]',
+      'div[@id="form-evaluation"][@class="panel-body collapse in"]',
+      'div[@class="tab-content"]',
+    ].join('/')
   end
 
   def form_xpath
     [
       form_panel_xpath,
+      'div[@id="new-evaluation"]',
       'form[@action="/evaluations"][@data-remote="true"][@method="post"]' \
-      '[@class="new_evaluation"]',
+      '[@id="new_evaluation"]',
     ].join('/')
+  end
+
+  def input_xpath
+    [
+      form_xpath,
+      'div[@class="row"]',
+      'div[@class="col-xs-6 form-group"]',
+    ].join('/')
+  end
+
+  def table_panel_xpath
+    container_xpath
   end
 
   def link_two_xpath
