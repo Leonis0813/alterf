fe6737a015bcea94852ce8839a71cca36bd67c15
Leonis0813 <Leonis.0813@gmail.com>
2021-07-06 15:22:58 +0000

    transform categorical data

--- app/assets/javascripts/evaluation/races/show.js
+++ app/assets/javascripts/evaluation/races/show.js
@@ -1,30 +1,19 @@
 import 'tablesorter';
 
 $(function() {
-  $('#table-evaluation-race-raw-data').tablesorter({
-    headers: {
-      3: { sorter: false },
-      4: { sorter: false },
-      7: { sorter: false },
-      13: { sorter: false },
-      15: { sorter: false },
-      17: { sorter: false },
-      21: { sorter: false },
-      22: { sorter: false },
-    }
-  });
-
-  $('#table-evaluation-race-feature').tablesorter({
-    headers: {
-      3: { sorter: false },
-      4: { sorter: false },
-      7: { sorter: false },
-      13: { sorter: false },
-      15: { sorter: false },
-      17: { sorter: false },
-      21: { sorter: false },
-      22: { sorter: false },
-    }
+  ['raw-data', 'feature'].forEach(function(type) {
+    $(`#table-evaluation-race-${type}`).tablesorter({
+      headers: {
+        3: { sorter: false },
+        4: { sorter: false },
+        7: { sorter: false },
+        13: { sorter: false },
+        15: { sorter: false },
+        17: { sorter: false },
+        21: { sorter: false },
+        22: { sorter: false },
+      }
+    });
   });
 
   const racewiseFeatures = [
@@ -41,6 +30,7 @@ $(function() {
     'weight_per',
     'win_times',
   ];
+
   racewiseFeatures.forEach(function(featureName) {
     const elements = $(`#table-evaluation-race-feature td.${featureName}`);
     const values = [];
@@ -48,13 +38,31 @@ $(function() {
       values.push(parseFloat(e.innerText));
     });
 
-    const scale = d3.scaleLinear()
-            .domain([Math.min(...values), Math.max(...values)])
-            .range([0, 1.0]);
+    const min = Math.min(...values);
+    const max = Math.max(...values);
+    const scale = d3.scaleLinear().domain([min, max]).range([0, 1.0]);
     const normalizedValues = values.map(value => scale(value));
 
     elements.each(function(i, e) {
-      e.innerText = normalizedValues[i];
+      e.innerText = min === max ? 0.0 : normalizedValues[i];
+    });
+  });
+
+  const mapping = {
+    direction: {'右': 0, '左': 1, '直': 2, '障': 3},
+    grade: {'N': 0, 'G1': 1, 'G2': 2, 'G3': 3, 'G': 4, 'J.G1': 5, 'J.G2': 6, 'J.G3': 7, 'L': 8, 'OP': 9},
+    place: {'中京': 0, '中山:': 1, '京都': 2, '函館': 3, '小倉': 4, '新潟': 5, '札幌': 6, '東京': 7, '福島': 8, '阪神': 9},
+    running_style: {'逃げ': 0, '先行': 1, '差し': 2, '追込': 3},
+    sex: {'牝': 0, '牡': 1, 'セ': 2},
+    track: {'芝': 0, 'ダート': 1, '障': 2},
+    weather: {'晴': 0, '曇': 1, '小雨': 2, '雨': 3, '小雪': 4, '雪': 5},
+  };
+
+  Object.keys(mapping).forEach(function(featureName) {
+    let featureMap = mapping[featureName];
+
+    $(`#table-evaluation-race-feature td.${featureName}`).each(function(i, e) {
+      e.innerText = featureMap[e.innerText];
     });
   });
 });
